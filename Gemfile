ce98b498f36bbf709727cbc0bd2da46800f1e0b9
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-01-11 15:02:15 -0700

    db drop issue resolved, the seed data and test data were mixing and causing errors

--- Gemfile
+++ Gemfile
@@ -32,12 +32,12 @@ gem 'jsonapi-serializer'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'faker'
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'factory_bot_rails'
   gem 'pry'
   gem 'rubocop-rails'
   gem 'rubocop-faker'
+  gem 'faker'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'teas/index'
-    end
-  end
-  namespace :api do
-    namespace :v1 do
-      get 'customers/index'
-    end
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :teas, only: :index
       resources :customers, only: :index do
         resources :subscriptions, only: %i[create update index]
       end
+      resources :teas, only: :index
     end
   end
 end
