9a11debfbd5b9d514627b4a4b659ee1ae8aa2ebc
Ryan Smith <106180616+RyanChrisSmith@users.noreply.github.com>
2023-01-11 13:40:37 -0700

    feat: endpoint to retrieve all customers, happy path tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'customers/index'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :customers do
+      resources :customers, only: :index do
         resources :subscriptions, only: %i[create update index]
       end
     end
